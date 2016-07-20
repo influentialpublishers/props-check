0a6ea827b9669b0f2913b3c091a99ebd61a88cd2
Nathan Sculli <scull7@gmail.com>
2016-07-20 01:01:44 -0700

    initial thoughts on the test.

---
+++ index.js
@@ -0,0 +1,10 @@
+const _     = require('ramda');
+const leven = require('leven');
+
+
+const PropCheck = _.curry((spec, target) => {
+
+});
+
+
+module.exports = PropCheck;
---
+++ test/index.spec.js
@@ -0,0 +1,42 @@
+/*eslint-env node, mocha*/
+const expect    = require('chai').expect;
+const PropCheck = require('../index');
+
+
+describe('prop-check/index.js', () => {
+
+  it('should be a function with an arity of 2', () => {
+
+    expect(PropCheck).to.be.a('function');
+    expect(PropCheck.length).to.eql(2);
+
+  });
+
+
+  it('should return a map with a list of possible corrections ' +
+  'if the key is not in the spec', () => {
+
+    const spec = {
+      read: 'Function'
+    , sanitize: '[Function]'
+    , validate: '[Function]'
+    , normalize: '[Function]'
+    };
+
+    const test = {
+      raed: () => null
+    , santize: [ () => null ]
+    , alvidate: [ () => null ]
+    , normalize: [ () => null ]
+    };
+
+    const actual = PropCheck(spec, test);
+
+    expect(actual.raed).to.deep.eql([ 'read' ]);
+    expect(actual.santize).to.deep.eql([ 'sanitize' ]);
+    expect(actual.alvidate).to.deep.eql([ 'alvidate' ]);
+    expect(actual.normalize).to.deep.eql([]);
+
+  });
+
+});
